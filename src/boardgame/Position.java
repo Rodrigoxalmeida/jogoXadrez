db7199188f6d69499bbf19d406b507af204b645d
Rodrigoxalmeida <Rodrigox.almeida@yahoo.com.br>
2020-03-20 22:44:25 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,39 @@
+package boardgame;
+
+public class Position {
+
+	private int row;
+	private int column;
+	
+	//construtor passando uma linha e coluna
+	public Position(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	// get e set
+	
+	public int getRow() {
+		return row;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	// toString para imprimir a posição na tela
+	@Override
+	public String toString() {
+		
+		return row + ", " + column;
+		
+		}
+	
+	
+	}
+
